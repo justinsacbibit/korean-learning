d25bc803c06cd44bfbbb79f89dd3f7005a7f6d0e
Justin Sacbibit <jsacbibit@gmail.com>
2019-04-14 22:01:47 -0700

    Updates

--- precache-manifest.2fe7f506c7428fb823cba123a758f436.js
+++ precache-manifest.2fe7f506c7428fb823cba123a758f436.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/korean-learning/static/js/runtime~main.f0570e49.js"
   },
   {
-    "revision": "d46b81e994ee2f963b03",
-    "url": "/korean-learning/static/js/main.eda9638f.chunk.js"
+    "revision": "b76c4290e26ade649254",
+    "url": "/korean-learning/static/js/main.d999776c.chunk.js"
   },
   {
     "revision": "9d3dd289c0e193bd9676",
     "url": "/korean-learning/static/js/2.fdfc0d62.chunk.js"
   },
   {
-    "revision": "d46b81e994ee2f963b03",
+    "revision": "b76c4290e26ade649254",
     "url": "/korean-learning/static/css/main.8b2eb5eb.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/korean-learning/static/css/2.e5f5df1a.chunk.css"
   },
   {
-    "revision": "f1ea70580175ab92fd971667cd76b98f",
+    "revision": "274c5cee433c6a9f3144f47e58d9003d",
     "url": "/korean-learning/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/korean-learning/precache-manifest.78a25e622afd14f9baeefddac3f7744f.js"
+  "/korean-learning/precache-manifest.2fe7f506c7428fb823cba123a758f436.js"
 );
 
 workbox.clientsClaim();
