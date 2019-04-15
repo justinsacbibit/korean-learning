78980b85613635c82383a83c74fb015270bfdc20
Justin Sacbibit <sacbibit@justins-mbp.attlocal.net>
2019-04-14 20:17:15 -0700

    Updates

---
+++ precache-manifest.2053dd7e52dc14d7628ca0c508fb829e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "714459f54b2d9427e7da",
+    "url": "/korean-learning/static/js/runtime~main.f0570e49.js"
+  },
+  {
+    "revision": "d505fb2c64558ccac38c",
+    "url": "/korean-learning/static/js/main.a3576b6c.chunk.js"
+  },
+  {
+    "revision": "9d3dd289c0e193bd9676",
+    "url": "/korean-learning/static/js/2.fdfc0d62.chunk.js"
+  },
+  {
+    "revision": "d505fb2c64558ccac38c",
+    "url": "/korean-learning/static/css/main.8b2eb5eb.chunk.css"
+  },
+  {
+    "revision": "9d3dd289c0e193bd9676",
+    "url": "/korean-learning/static/css/2.e5f5df1a.chunk.css"
+  },
+  {
+    "revision": "6ae10c88603daf6d7237f856cfc2fe58",
+    "url": "/korean-learning/index.html"
+  }
+];
